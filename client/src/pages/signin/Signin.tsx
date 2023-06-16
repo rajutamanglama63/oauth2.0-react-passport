fe75b418a622aed052bf07bd586775f45a9adbeb
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-06-16 14:46:45 +0545

    feat: github login btn added

--- client/src/pages/signin/Signin.tsx
+++ client/src/pages/signin/Signin.tsx
@@ -1,9 +1,14 @@
 import styles from "./signin.module.css";
 import Google from "../../img/google.png";
+import Github from "../../img/github.png";
 
 const Signin = () => {
   const google = () => {
     window.open("http://localhost:5000/auth/google", "_self");
+  };
+
+  const github = () => {
+    // window.open("http://localhost:5000/auth/google", "_self");
   };
   return (
     <div className={styles.signin}>
@@ -13,9 +18,20 @@ const Signin = () => {
         <input className={styles.input_field} placeholder="password" />
         <button className={styles.submitBtn}>Signin</button>
       </form>
-      <div className={styles.loginButton} onClick={google}>
+      <div
+        className={`${styles.loginButton} ${styles.google}`}
+        onClick={google}
+      >
         <img src={Google} alt="google" className={styles.icon} />
         Google
+      </div>
+
+      <div
+        className={`${styles.loginButton} ${styles.github}`}
+        onClick={github}
+      >
+        <img src={Github} alt="google" className={styles.icon} />
+        Github
       </div>
     </div>
   );
